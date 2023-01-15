113edeb130a765dac0887857a957f2c3091037aa
Artem <mart_22@ukr.net>
2023-01-15 23:11:26 +0200

    Edit blocksStyles.js

--- src/js/modules/blocksStyles.js
+++ src/js/modules/blocksStyles.js
@@ -4,4 +4,14 @@ export default function blockStyles() {
 	} else {
 		document.body.classList.add('inner');
 	}
+
+	if (document.querySelector('.order-form .list-6')) {
+		document.querySelector('.order-form .list-6').addEventListener('input', function (e) {
+			const target = e.target;
+
+			if (target.tagName === 'INPUT' && target.classList.contains('order-form__number')) {
+				target.value = e.target.value.replace(/\D/g, '');
+			}
+		});
+	}
 }
