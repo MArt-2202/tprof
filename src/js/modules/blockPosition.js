f961ac78b6e74c69f73a9ebe46b1366d1673167d
Artem <mart_22@ukr.net>
2023-01-19 14:21:13 +0200

    Add header blocks position in blockPosition.js

--- src/js/modules/blockPosition.js
+++ src/js/modules/blockPosition.js
@@ -1,6 +1,10 @@
 // BLOCK POSITION
 export default function blockPosition() {
-	// const bl1 = document.querySelector('.header__bl-2');
+	const bl1 = document.querySelector('.header__bl-2'),
+		bl2 = document.querySelector('.header__bl-3'),
+		bl3 = document.querySelector('.toggle__content div'),
+		bl4 = document.querySelector('.contacts-wrapper'),
+		bl5 = document.querySelector('.navigation');
 
 	if (window.matchMedia('(min-width: 361px)').matches) {
 		document.body.classList.remove('max-360');
@@ -35,54 +39,44 @@ export default function blockPosition() {
 		}
 	}
 
-	if (window.matchMedia('(min-width: 1025px)').matches) {
-		document.body.classList.remove('max-360', 'max-576', 'max-768', 'max-1024');
+	if (window.matchMedia('(min-width: 1091px)').matches) {
+		document.body.classList.remove('max-360', 'max-576', 'max-768', 'max-1090');
 
-		if (!document.body.classList.contains('min-1025')) {
-			document.body.classList.add('min-1025');
+		if (!document.body.classList.contains('min-1091')) {
+			document.body.classList.add('min-1091');
 
-			if (document.querySelector('.min-1025')) {
+			if (document.querySelector('.min-1091')) {
+				if (bl1 && bl5) {
+					bl1.prepend(bl5);
+				}
+				if (bl2 && bl4) {
+					bl2.prepend(bl4);
+				}
 			}
 		}
 	}
 
-	if (window.matchMedia('(min-width: 1201px)').matches) {
-		document.body.classList.remove('max-360', 'max-576', 'max-768', 'max-1024', 'max-1200');
+	if (window.matchMedia('(max-width: 1090px)').matches) {
+		document.body.classList.remove('min-1091');
 
-		if (!document.body.classList.contains('min-1201')) {
-			document.body.classList.add('min-1201');
+		if (!document.body.classList.contains('max-1090')) {
+			document.body.classList.add('max-1090');
 
-			if (document.querySelector('.min-1201')) {
-			}
-		}
-	}
-
-	if (window.matchMedia('(max-width: 1200px)').matches) {
-		document.body.classList.remove('min-1201');
-
-		if (!document.body.classList.contains('max-1200')) {
-			document.body.classList.add('max-1200');
-
-			if (document.querySelector('.max-1200')) {
-			}
-		}
-	}
-
-	if (window.matchMedia('(max-width: 1024px)').matches) {
-		document.body.classList.remove('min-1201', 'min-1025');
-
-		if (!document.body.classList.contains('max-1024')) {
-			document.body.classList.add('max-1024');
-
-			if (document.querySelector('.max-1024')) {
-				if (document.querySelector('.max-1024')) {
+			if (document.querySelector('.max-1090')) {
+				if (document.querySelector('.max-1090')) {
+					if (bl3 && bl4) {
+						bl3.append(bl4);
+					}
+					if (bl3 && bl5) {
+						bl3.prepend(bl5);
+					}
 				}
 			}
 		}
 	}
 
 	if (window.matchMedia('(max-width: 768px)').matches) {
-		document.body.classList.remove('min-1201', 'min-1025', 'min-769');
+		document.body.classList.remove('min-1091', 'min-769');
 
 		if (!document.body.classList.contains('max-768')) {
 			document.body.classList.add('max-768');
@@ -93,7 +87,7 @@ export default function blockPosition() {
 	}
 
 	if (window.matchMedia('(max-width: 576px)').matches) {
-		document.body.classList.remove('min-1201', 'min-1025', 'min-769', 'min-577', 'min-361');
+		document.body.classList.remove('min-1091', 'min-769', 'min-577', 'min-361');
 
 		if (!document.body.classList.contains('max-576')) {
 			document.body.classList.add('max-576');
@@ -104,7 +98,7 @@ export default function blockPosition() {
 	}
 
 	if (window.matchMedia('(max-width: 360px)').matches) {
-		document.body.classList.remove('min-1201', 'min-1025', 'min-769', 'min-361');
+		document.body.classList.remove('min-1091', 'min-769', 'min-361');
 
 		if (!document.body.classList.contains('max-360')) {
 			document.body.classList.add('max-360');
