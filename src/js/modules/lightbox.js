068c3fee63719b07a08036a8ec14f60e77f27f46
Artem <mart_22@ukr.net>
2023-01-19 20:32:46 +0200

    Add check user agent in lightbox.js

--- src/js/modules/lightbox.js
+++ src/js/modules/lightbox.js
@@ -2,19 +2,24 @@ import sliders from './sliders';
 
 export default function lightbox({ container, selector, plugins, speed, zoom, download, mode }) {
 	if (document.querySelector(container)) {
-		document.querySelector(container).addEventListener('lgInit', (event) => {
-			sliders();
-		});
+		if (document.querySelector('.desktop-user-agent')) {
+			document.querySelector(container).addEventListener('lgInit', (event) => {
+				sliders();
+			});
 
-		lightGallery(document.querySelector(container), {
-			// licenseKey: '0000-0000-000-0000',
-			selector,
-			plugins,
-			speed,
-			zoom,
-			download,
-			mode,
-			// counter: false,
-		});
+			lightGallery(document.querySelector(container), {
+				// licenseKey: '0000-0000-000-0000',
+				selector,
+				plugins,
+				speed,
+				zoom,
+				download,
+				mode,
+				// counter: false,
+			});
+		}
+		if (document.querySelector('.mobile-user-agent')) {
+			sliders();
+		}
 	}
 }
