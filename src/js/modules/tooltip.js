399456c2491a24a3520815b92ace9cc376e36866
Artem <mart_22@ukr.net>
2023-01-23 14:52:06 +0200

    Edit tooltip position in tooltip.js

--- src/js/modules/tooltip.js
+++ src/js/modules/tooltip.js
@@ -12,6 +12,25 @@ export default function tooltip({ wrapper, tag, tooltipButton, addClass, showEve
 
 				tooltip.style.marginTop = `-${tooltip.offsetHeight}px`;
 
+				if (document.querySelector('.desktop-user-agent')) {
+					if (tooltip.offsetWidth < document.querySelector(`[${wrapper}]`).offsetLeft) {
+						tooltip.classList.remove('position-left');
+						tooltip.classList.add('position-right');
+					}
+					if (
+						tooltip.offsetWidth + tooltip.offsetLeft >
+						document.querySelector(`[${wrapper}]`).offsetWidth
+					) {
+						tooltip.classList.add('width-full');
+					} else {
+						tooltip.classList.remove('position-right');
+						tooltip.classList.add('position-left');
+					}
+				}
+				if (document.querySelector('.mobile-user-agent')) {
+					tooltip.classList.remove('position-left', 'position-right');
+				}
+
 				btn.addEventListener(showEvent, () => {
 					tooltip.classList.add(`${addClass}`);
 				});
